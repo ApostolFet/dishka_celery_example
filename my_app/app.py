940eeb5708a60ec02a0c1b5ea0886e1a7a4524ad
Apostol Fet <maksim170901@gmail.com>
2024-11-30 18:30:38 +0300

    test with retry and close app container

--- my_app/app.py
+++ my_app/app.py
@@ -1,11 +1,14 @@
-from celery import Celery
+from collections.abc import Iterator
+from logging import Logger
+
+from celery import Celery, Task, current_app
 from celery.result import AsyncResult
+from celery.signals import worker_process_shutdown
 from celery.utils.log import get_task_logger
 from dishka import Provider, Scope, make_container, provide
 from dishka.integrations.celery import (
     DishkaTask,
     FromDishka,
-    inject,
     setup_dishka,
 )
 from flask import Flask, Response, jsonify, request
@@ -19,6 +22,14 @@ celery_app = Celery(
 logger = get_task_logger(__name__)
 
 
+class MyClass:
+    def __init__(self, logger: Logger):
+        self._logger = logger
+
+    def log(self, message: str):
+        self._logger.info(message)
+
+
 @celery_app.on_after_configure.connect()
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(10.0, test.s("forever"), name="forever")
@@ -26,21 +37,48 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 @celery_app.task()
-def test(input_str: str, my_str: FromDishka[str]):
-    logger.info("%s %s", my_str, input_str)
+def test(input_str: str, my_str: FromDishka[str], my_class: FromDishka[MyClass]):
+    my_class.log(f"{my_str} {input_str}")
 
 
-@celery_app.task()
-def add_my_int(x: int, *, y: FromDishka[int]):
+@celery_app.task(bind=True)
+def add_my_int(
+    task: Task,
+    x: int,
+    *,
+    y: FromDishka[int],
+    my_class: FromDishka[MyClass],
+):
+    if task.request.retries < 1:
+        task.retry(countdown=5)
+
     z = x + y
-    logger.info(z)
+    my_class.log(str(z))
     return z
 
 
 class MyProveder(Provider):
-    scope = Scope.REQUEST
-    my_int = provide(lambda self: 17, provides=int)
-    my_str = provide(lambda self: "dishka", provides=str)
+    @provide(scope=Scope.APP)
+    def get_my_str(self) -> Iterator[str]:
+        logger.info("init my str")
+        yield "dishka"
+        logger.info("close my str")
+
+    @provide(scope=Scope.REQUEST)
+    def get_my_int(self) -> Iterator[int]:
+        logger.info("init my int")
+        yield 17
+        logger.info("close my int")
+
+    @provide(scope=Scope.REQUEST)
+    def get_my_class(self) -> MyClass:
+        return MyClass(logger)
+
+
+@worker_process_shutdown.connect()
+def close_dishka(*args, **kwargs):
+    container: Container = current_app.conf["dishka_container"]
+    container.close()
 
 
 container = make_container(MyProveder())
